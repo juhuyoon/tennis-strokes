65409a20ad28ba6db376a1512e0b49a4857c80ce
jyoon3 <jyoon3@2u.com>
2023-05-12 14:01:43 -0400

    added on process.env.port to deploy to heroku!

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const controllers = require('./controllers');
 const sequelize = require('./config/connection');
-const PORT = 3001;
+const exphbs = require('express-handlebars');
+// We need this to deploy to the server so that it BINDS to the nearest available port number
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+// To access the public/front-end content!
+app.use(express.static('public'));
+
+// TO USE HANDLEBARSSSSSSSSS
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set("view engine", "handlebars");
 
 // THIS IS NEEDED TO DO POST REQUESTS!!!
 app.use(express.json());
