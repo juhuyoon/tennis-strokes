ac180682a6a797de76bfae114a4b8a682ff3d1d0
jyoon3 <jyoon3@2u.com>
2023-05-12 12:57:41 -0400

    wrote a test get request, it works

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const routes = require('./routes');
 const PORT = 3001;
 const app = express();
 
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log("DAD WILL COME BACK SOON");
